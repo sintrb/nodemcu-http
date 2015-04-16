27df1d6e8074a686de9ff64e8f3ef79f7e7d0a4f
RobinTang <sintrb@gmail.com>
2015-04-16 21:00:49 +0800

    mod

--- http_server_example.lua
+++ http_server_example.lua
@@ -9,10 +9,10 @@ Server = require("http_server")
 
 
 local serv = Server.New('0.0.0.0', 9999)
-Server.On(serv, "/", function(cxt)
+Server.On(serv, "^/$", function(cxt)
 	return '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Hello World</title></head><body>this is a demo.</body></html>'
 end)
-Server.On(serv, "/hi", function(cxt)
+Server.On(serv, "^/hi$", function(cxt)
 	return "say hi"
 end)
 
